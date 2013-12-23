4a45353f5866fd83e3fa6019af2aedcf0b302910
Joshua Adams <josh.adams@annkissam.com>
2013-12-23 12:18:49 -0800

    fixed overlap "where" append

--- lib/acts_as_span/span_klass/overlap.rb
+++ lib/acts_as_span/span_klass/overlap.rb
@@ -8,7 +8,7 @@ module ActsAsSpan
           overlap_scope = klass.where( ["(#{table_name}.#{start_date_field} IS NULL OR :end_date IS NULL OR #{table_name}.#{start_date_field} <= :end_date) AND (#{table_name}.#{end_date_field} IS NULL OR :start_date IS NULL OR :start_date <= #{table_name}.#{end_date_field})", { :start_date => test_span.start_date, :end_date => test_span.end_date } ] )
 
           if !test_span.new_record? && test_span.span_klass == klass
-            overlap_scope.where( ["#{table_name}.id <> :id", { :id => test_span.span_model.id } ] )
+            overlap_scope = overlap_scope.where( ["#{table_name}.id <> :id", { :id => test_span.span_model.id } ] )
           end
 
           overlap_scope
