ef44ddf97ec74bef59a5db6e4e6c5209f1c66eed
Eric Sullivan <eric.sullivan@annkissam.com>
2014-05-21 14:06:50 -0400

    remove deprecation warning

--- lib/acts_as_span/span_instance/status.rb
+++ lib/acts_as_span/span_instance/status.rb
@@ -2,55 +2,53 @@ module ActsAsSpan
   class SpanInstance
     module Status
       extend ActiveSupport::Concern
-      
-      module InstanceMethods
-        def span_status(query_date = Date.today)
-          if future?(query_date)
-            :future
-          elsif expired?(query_date)
-            :expired
-          elsif current?(query_date)
-            :current
-          else
-            :unknown
-          end
-        end
 
-        alias_method :span_status_on, :span_status
-
-        def span_status_to_s(query_date = Date.today)
-          case span_status(query_date)
-          when :future
-            "Future"
-          when :expired
-            "Expired"
-          when :current
-            "Current"
-          when :unknown
-            "Unknown"
-          end
+      def span_status(query_date = Date.today)
+        if future?(query_date)
+          :future
+        elsif expired?(query_date)
+          :expired
+        elsif current?(query_date)
+          :current
+        else
+          :unknown
         end
-
-        alias_method :span_status_to_s_on, :span_status_to_s
-        
-        def current?(query_date = Date.today)
-          !future?(query_date) && !expired?(query_date)
-        end
-        
-        alias_method :current_on?, :current?
-
-        def future?(query_date = Date.today)
-          start_date && start_date > query_date
-        end
-        
-        alias_method :future_on?, :future?
-
-        def expired?(query_date = Date.today)
-          end_date && end_date < query_date
-        end
-        
-        alias_method :expired_on?, :expired?
       end
+
+      alias_method :span_status_on, :span_status
+
+      def span_status_to_s(query_date = Date.today)
+        case span_status(query_date)
+        when :future
+          "Future"
+        when :expired
+          "Expired"
+        when :current
+          "Current"
+        when :unknown
+          "Unknown"
+        end
+      end
+
+      alias_method :span_status_to_s_on, :span_status_to_s
+
+      def current?(query_date = Date.today)
+        !future?(query_date) && !expired?(query_date)
+      end
+
+      alias_method :current_on?, :current?
+
+      def future?(query_date = Date.today)
+        start_date && start_date > query_date
+      end
+
+      alias_method :future_on?, :future?
+
+      def expired?(query_date = Date.today)
+        end_date && end_date < query_date
+      end
+
+      alias_method :expired_on?, :expired?
     end
   end
 end
--- lib/acts_as_span/span_klass/overlap.rb
+++ lib/acts_as_span/span_klass/overlap.rb
@@ -3,16 +3,14 @@ module ActsAsSpan
     module Overlap
       extend ActiveSupport::Concern
 
-      module InstanceMethods
-        def overlap(test_span)
-          overlap_scope = klass.where( ["(#{table_name}.#{start_date_field} IS NULL OR :end_date IS NULL OR #{table_name}.#{start_date_field} <= :end_date) AND (#{table_name}.#{end_date_field} IS NULL OR :start_date IS NULL OR :start_date <= #{table_name}.#{end_date_field})", { :start_date => test_span.start_date, :end_date => test_span.end_date } ] )
+      def overlap(test_span)
+        overlap_scope = klass.where( ["(#{table_name}.#{start_date_field} IS NULL OR :end_date IS NULL OR #{table_name}.#{start_date_field} <= :end_date) AND (#{table_name}.#{end_date_field} IS NULL OR :start_date IS NULL OR :start_date <= #{table_name}.#{end_date_field})", { :start_date => test_span.start_date, :end_date => test_span.end_date } ] )
 
-          if !test_span.new_record? && test_span.span_klass == klass
-            overlap_scope = overlap_scope.where( ["#{table_name}.id <> :id", { :id => test_span.span_model.id } ] )
-          end
-
-          overlap_scope
+        if !test_span.new_record? && test_span.span_klass == klass
+          overlap_scope = overlap_scope.where( ["#{table_name}.id <> :id", { :id => test_span.span_model.id } ] )
         end
+
+        overlap_scope
       end
     end
   end
