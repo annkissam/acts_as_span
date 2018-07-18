98c1ae393398aa66dda671e7722b1fbacd49ea65
Florin Popa <pp.flrn@gmail.com>
2018-07-18 13:20:38 +0300

    Small refactoring, improve specs added bug spec

--- spec/lib/within_parent_date_span_validator_spec.rb
+++ spec/lib/within_parent_date_span_validator_spec.rb
@@ -11,30 +11,80 @@ RSpec.describe ActsAsSpan::WithinParentDateSpanValidator do
   let!(:mama) { Mama.create(start_date: mama_start_date, end_date: mama_end_date) }
   let!(:papa) { Papa.create(start_date: papa_start_date, end_date: papa_end_date) }
 
-  let(:mama_start_date) { Date.current - 30.years }
-  let(:mama_end_date) { Date.current + 70.years  }
+  let(:thirty_years_ago) { Date.current - 30.years }
+  let(:seventy_years_in_the_future) { Date.current + 70.years }
 
-  let(:papa_start_date) { mama_start_date + 1.year }
-  let(:papa_end_date) { mama_end_date - 1.year }
+  let(:mama_start_date) { thirty_years_ago }
+  let(:mama_end_date) { seventy_years_in_the_future }
+
+  let(:papa_start_date) { thirty_years_ago + 1.year }
+  let(:papa_end_date) { seventy_years_in_the_future - 1.year }
 
   describe 'an object that validates against a single parent' do
     let(:child_class) { OneParentChild }
 
-    context 'valid case' do
-      let(:new_child_start_date) { mama_start_date  }
+    context 'inside parent date span' do
+      let(:new_child_start_date) { mama_start_date }
       let(:new_child_end_date) { mama_end_date }
 
-      it 'allows date spans that lie within all respective parental date span' do
+      it do
         expect(new_child).to be_valid
       end
     end
 
-    context 'invalid cases' do
+    context 'child_record_without_start_date' do
+      let(:new_child_start_date) { nil }
+      let(:new_child_end_date) { mama_end_date }
+
+      it do
+        expect(new_child).to be_invalid
+      end
+    end
+
+    context 'child_record_without_start_date' do
+      let(:new_child_start_date) { mama_start_date }
+      let(:new_child_end_date) { nil }
+
+      it do
+        expect(new_child).to be_invalid
+      end
+    end
+
+    context 'child_record_started_before_parent_record' do
       let(:new_child_start_date) { mama_start_date - 1 }
+      let(:new_child_end_date) { mama_end_date }
+
+      it do
+        expect(new_child).to be_invalid
+      end
+    end
+
+    context 'child_record_ended_after_parent_record' do
+      let(:new_child_start_date) { mama_start_date }
       let(:new_child_end_date) { mama_end_date + 1 }
 
-      it 'does not allow date spans that lie outside of respective parental date span' do
-        expect(new_child).not_to be_valid
+      it do
+        expect(new_child).to be_invalid
+      end
+    end
+
+    context 'with unassigned parent start_date' do
+      let(:new_child_start_date) { thirty_years_ago }
+      let(:new_child_end_date) { mama_end_date }
+      let(:mama_start_date) { nil }
+
+      it do
+        expect(new_child).to be_valid
+      end
+    end
+
+    context 'with unassigned parent end_date' do
+      let(:new_child_start_date) { mama_start_date }
+      let(:new_child_end_date) { seventy_years_in_the_future }
+      let(:mama_end_date) { nil }
+
+      it do
+        expect(new_child).to be_valid
       end
     end
   end
