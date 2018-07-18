3f169bfb79f7e71e4bc5d1feace0572acd1c25d0
Florin Popa <pp.flrn@gmail.com>
2018-07-18 13:33:54 +0300

    bump gem version

--- lib/acts_as_span/version.rb
+++ lib/acts_as_span/version.rb
@@ -2,11 +2,11 @@ module ActsAsSpan
   module VERSION
     MAJOR = 0
     MINOR = 0
-    TINY  = 5
+    TINY  = 6
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
-    
+
     SUMMARY = "acts_as_span #{STRING}"
   end
 end
