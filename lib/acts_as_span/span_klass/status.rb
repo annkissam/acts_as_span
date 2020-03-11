fb04c5ec90d293746d2a5438e4c80625f71cbe89
Octavian Neamțu <octavian.neamtu@annkissam.com>
2020-03-11 10:01:53 -0400

    Add ransack scopes integration.

--- lib/acts_as_span/span_klass/status.rb
+++ lib/acts_as_span/span_klass/status.rb
@@ -44,6 +44,23 @@ module ActsAsSpan
         end
 
         alias_method :current_or_future, :current_or_future_on
+
+        if defined?(Ransack)
+          def ransackable_scopes(_auth_object = nil)
+            super + %i[
+              current
+              current_on
+              future
+              future_on
+              expired
+              expired_on
+              past
+              past_on
+              current_or_future
+              current_or_future_on
+            ]
+          end
+        end
       end
     end
   end
