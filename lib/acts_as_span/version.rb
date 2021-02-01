d1ec1a4029907dc34425d4986babc2feb5b6a494
is <isaac.ball@annkissam.com>
2021-02-01 14:36:01 -0500

    Bump version

--- lib/acts_as_span/version.rb
+++ lib/acts_as_span/version.rb
@@ -1,8 +1,10 @@
+# frozen_string_Literal: true
+
 module ActsAsSpan
   module VERSION
     MAJOR = 1
-    MINOR = 1
-    TINY  = 2
+    MINOR = 2
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
