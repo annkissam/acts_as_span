8ca92f4f8f16c06ab4e6b7aef3bba31ce30aeac8
is <isaac.ball@annkissam.com>
2021-03-17 14:27:44 -0400

    Expose the arel that the span scopes use
    No more rewriting "current" in applications! (unless it's functionally
    different)
    
    Also adds missing SpanKlass:Status specs
    
    The arel methods are tested by being used in the tested methods -
    further testing could be warranted, if some use cases come out of the
    woodwork. I take requests.

--- lib/acts_as_span/span_klass/status.rb
+++ lib/acts_as_span/span_klass/status.rb
@@ -1,58 +1,75 @@
+# frozen_string_Literal: true
+
 require 'active_support'
 
 module ActsAsSpan
   class SpanKlass
+    # Defines class-level methods for relative span scopes.
     module Status
       extend ActiveSupport::Concern
 
       included do
         def current(query_date = Date.current)
           klass.where(
-            current_condition(query_date: query_date, table: arel_table)
+            current_condition(query_date: query_date, table: arel_table),
           )
         end
 
         alias_method :current_on, :current
 
         def future(query_date = Date.current)
-          klass.where(arel_table[start_field].gt(query_date))
+          klass.where(
+            future_condition(query_date: query_date, table: arel_table),
+          )
         end
 
         alias_method :future_on, :future
 
         def expired(query_date = Date.current)
-          klass.where(arel_table[end_field].lt(query_date))
+          klass.where(
+            expired_condition(query_date: query_date, table: arel_table),
+          )
         end
 
         alias_method :expired_on, :expired
         alias_method :past_on, :expired
         alias_method :past, :expired
 
-
         def current_or_future_on(query_date = Date.current)
-          klass.where(
-            arel_table[start_field].lteq(query_date).
-            and(
-              arel_table[end_field].eq(nil).
-              or(arel_table[end_field].gteq(query_date))
-            ).
-            or(arel_table[start_field].gt(query_date))
-          )
+          current_cond =
+            current_condition(query_date: query_date, table: arel_table)
+          future_cond =
+            future_condition(query_date: query_date, table: arel_table)
+
+          klass.where(current_cond.or(future_cond))
         end
 
         alias_method :current_or_future, :current_or_future_on
 
-        private
-
-        # returns an Arel node usable within an ActiveRecord `where` clause
+        # The *_condition methods return Arel nodes for composition purposes.
+        #
+        # They accept keyword arguments:
+        #
+        # * query_date: a Date object, the reference date
+        # * table: an Arel table, the table whose columns will be used
+        #
+        # `table` can also be Arel::Nodes::TableAlias
         def current_condition(query_date:, table:)
-          start_col = arel_table[start_field]
-          end_col = arel_table[end_field]
+          start_col = table[start_field]
+          end_col = table[end_field]
 
           start_condition = start_col.lteq(query_date).or(start_col.eq(nil))
           end_condition = end_col.eq(nil).or(end_col.gteq(query_date))
 
           start_condition.and(end_condition)
+        end
+
+        def expired_condition(query_date:, table:)
+          table[end_field].lt(query_date)
+        end
+
+        def future_condition(query_date:, table:)
+          table[start_field].gt(query_date)
         end
       end
     end
--- spec/lib/span_klass/status_spec.rb
+++ spec/lib/span_klass/status_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe ActsAsSpan::SpanKlass::Status do
 
   let!(:today) { Date.current }
 
-  describe '.current' do
-    let!(:record) do
-      span_klass.create(
-        start_date: today - 1.week,
-        end_date: today + 1.week,
-      )
-    end
+  let!(:record) do
+    span_klass.create(
+      start_date: today - 1.week,
+      end_date: today + 1.week,
+    )
+  end
 
+  describe '.current' do
     subject { span_klass.current(query_date) }
 
     context "when query_date is within the record's span" do
@@ -27,6 +27,72 @@ RSpec.describe ActsAsSpan::SpanKlass::Status do
       let(:query_date) { record.start_date - 1.week }
 
       it { is_expected.to be_empty }
+    end
+
+    context "when query_date is after the record's span" do
+      let(:query_date) { record.end_date + 1.week }
+
+      it { is_expected.to be_empty }
+    end
+  end
+
+  describe '.current_or_future' do
+    subject { span_klass.current_or_future(query_date) }
+
+    context "when query_date is within the record's span" do
+      let(:query_date) { record.start_date + 3.days }
+
+      it { is_expected.not_to be_empty }
+    end
+
+    context "when query_date is before the record's span" do
+      let(:query_date) { record.start_date - 1.week }
+
+      it { is_expected.not_to be_empty }
+    end
+
+    context "when query_date is after the record's span" do
+      let(:query_date) { record.end_date + 1.week }
+
+      it { is_expected.to be_empty }
+    end
+  end
+
+  describe '.expired' do
+    subject { span_klass.expired(query_date) }
+
+    context "when query_date is within the record's span" do
+      let(:query_date) { record.start_date + 3.days }
+
+      it { is_expected.to be_empty }
+    end
+
+    context "when query_date is before the record's span" do
+      let(:query_date) { record.start_date - 1.week }
+
+      it { is_expected.to be_empty }
+    end
+
+    context "when query_date is after the record's span" do
+      let(:query_date) { record.end_date + 1.week }
+
+      it { is_expected.not_to be_empty }
+    end
+  end
+
+  describe '.future' do
+    subject { span_klass.future(query_date) }
+
+    context "when query_date is within the record's span" do
+      let(:query_date) { record.start_date + 3.days }
+
+      it { is_expected.to be_empty }
+    end
+
+    context "when query_date is before the record's span" do
+      let(:query_date) { record.start_date - 1.week }
+
+      it { is_expected.not_to be_empty }
     end
 
     context "when query_date is after the record's span" do
