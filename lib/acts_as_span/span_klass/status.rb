8f718ae4baf14aa7414065cb318e8ca3f4c15463
is <isaac.ball@annkissam.com>
2021-03-18 13:20:49 -0400

    Rejigger SpanKlass::Status documentation
    [ci skip]

--- lib/acts_as_span/span_klass/status.rb
+++ lib/acts_as_span/span_klass/status.rb
@@ -5,10 +5,23 @@ require 'active_support'
 module ActsAsSpan
   class SpanKlass
     # Defines class-level methods for relative span scopes.
+    #
+    # The *_condition methods return Arel nodes for composition purposes.
+    #
+    # They accept keyword arguments:
+    #
+    # * query_date: a Date object, the reference date
+    # * table: an Arel table, the table whose columns will be used
+    #
+    # `table` can also be Arel::Nodes::TableAlias
     module Status
       extend ActiveSupport::Concern
 
       included do
+        # Records whose spans include the query date.
+        # If no query date is given, the current date is the query date.
+        # Date span is _inclusive_: if the query date is equal to the
+        #   span start or end date, that span counts as current.
         def current(query_date = Date.current)
           klass.where(
             current_condition(query_date: query_date, table: arel_table),
@@ -17,6 +30,8 @@ module ActsAsSpan
 
         alias_method :current_on, :current
 
+        # Records whose spans start strictly after the query date.
+        # If no query date is given, the current date is the query date.
         def future(query_date = Date.current)
           klass.where(
             future_condition(query_date: query_date, table: arel_table),
@@ -25,6 +40,8 @@ module ActsAsSpan
 
         alias_method :future_on, :future
 
+        # Records whose spans end strictly before the query date.
+        # If no query date is given, the current date is the query date.
         def expired(query_date = Date.current)
           klass.where(
             expired_condition(query_date: query_date, table: arel_table),
@@ -35,6 +52,10 @@ module ActsAsSpan
         alias_method :past_on, :expired
         alias_method :past, :expired
 
+        # Records whose spans include the query date, or start after the query
+        #   date.
+        # If no query date is given, the current date is the query date.
+        # Records are excluded from the result only if they are expired.
         def current_or_future_on(query_date = Date.current)
           current_cond =
             current_condition(query_date: query_date, table: arel_table)
@@ -46,14 +67,6 @@ module ActsAsSpan
 
         alias_method :current_or_future, :current_or_future_on
 
-        # The *_condition methods return Arel nodes for composition purposes.
-        #
-        # They accept keyword arguments:
-        #
-        # * query_date: a Date object, the reference date
-        # * table: an Arel table, the table whose columns will be used
-        #
-        # `table` can also be Arel::Nodes::TableAlias
         def current_condition(query_date:, table:)
           start_col = table[start_field]
           end_col = table[end_field]
