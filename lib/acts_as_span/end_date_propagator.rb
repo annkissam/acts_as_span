e6b0523d2bf448c0778d023ac76b6b2c11e342ae
is <isaac.ball@annkissam.com>
2021-03-30 17:25:39 -0400

    Fix constant name typo

--- lib/acts_as_span/end_date_propagator.rb
+++ lib/acts_as_span/end_date_propagator.rb
@@ -92,7 +92,7 @@ module ActsAsSpan
       # NOTE: Rails < 6.1 support
       # Errors are an array of Error objects in Rails 6.1 +
       if ActiveRecord::VERSION::MAJOR > 5 ||
-         (ActiveRecord::Version::MAJOR == 6 && ActiveRecord::VERSION::MINOR > 1)
+         (ActiveRecord::VERSION::MAJOR == 6 && ActiveRecord::VERSION::MINOR > 1)
         add_errors(result.errors)
       else
         add_rails_5_errors(result.errors)
